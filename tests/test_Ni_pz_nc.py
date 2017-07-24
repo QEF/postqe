cd7e7f522bbaa1280a2a8f9a54fb56a655038f6e
Davide Brunato <brunato@sissa.it>
2017-07-24 16:16:18 +0200

    Fixed readutils/read_pseudo_file and adapted a test file:
        modified:   postqe/readutils.py
        modified:   tests/test_Ni_pz_nc.py

--- tests/test_Ni_pz_nc.py
+++ tests/test_Ni_pz_nc.py
@@ -1,77 +1,76 @@
 #!/usr/bin/env python3
 #encoding: UTF-8
 
-
 import numpy as np
-from pp import get_from_xml
-from readutils import ( read_charge_file_hdf5,
-    read_wavefunction_file_hdf5, write_charge, create_header
-)
+from postqe.pp import get_from_xml
+from postqe.readutils import read_charge_file_hdf5, write_charge, create_header
+from postqe.compute_vs import compute_v_bare, compute_v_h, compute_v_xc
+from postqe.pyqe import pyqe_getcelldms
+from postqe.xmldata import PWData
 
 print ('Hello')
 
-from compute_vs import compute_v_bare, compute_v_h, compute_v_xc
-from pyqe import pyqe_getcelldms
 
 # get some needed values from the xml output
-ecutwfc, ecutrho, ibrav, alat, a, b, functional, atomic_positions, atomic_species, \
-nat, ntyp, nspin, noncolin, pseudodir, nr, nr_smooth = get_from_xml("../tests/Ni_pz_nc/Ni.xml")
-celldms = pyqe_getcelldms(alat, a[0], a[1], a[2], ibrav)
+data = PWData("./Ni_pz_nc/Ni.xml", '../postqe/schemas/qes.xsd')
+celldms = pyqe_getcelldms(data.alat, data.a[0], data.a[1], data.a[2], data.ibrav)
 
-pseudodir = "../tests/Ni_pz_nc/"
-charge_file = "../tests/Ni_pz_nc/charge-density.hdf5"
+pseudodir = "./Ni_pz_nc/"
+charge_file = "./Ni_pz_nc/charge-density.hdf5"
 
-charge, chargediff = read_charge_file_hdf5(charge_file, nr)
-header = create_header("Ni", nr, nr_smooth, ibrav, celldms, nat, ntyp, atomic_species, atomic_positions)
+charge, chargediff = read_charge_file_hdf5(charge_file, data.nr)
+header = create_header("Ni", data.nr, data.nr_smooth, data.ibrav, celldms, data.nat,
+                       data.ntyp, data.atomic_species, data.atomic_positions)
 
 # TESTS
 
 # plot_num = 0
-write_charge('../tests/Ni_pz_nc/postqeout0', charge, header)
+write_charge('./Ni_pz_nc/postqeout0', charge, header)
 
 # plot_num = 6
-write_charge('../tests/Ni_pz_nc/postqeout6', chargediff, header)
+write_charge('./Ni_pz_nc/postqeout6', chargediff, header)
 
 # plot_num = 2
 v_bare = compute_v_bare(
-    ecutrho, alat, a[0], a[1], a[2], nr, atomic_positions, atomic_species, pseudodir
+    data.ecutrho, data.alat, data.a[0], data.a[1], data.a[2], data.nr,
+    data.atomic_positions, data.atomic_species, pseudodir
 )
-write_charge('../tests/Ni_pz_nc/postqeout2', v_bare, header)
+write_charge('./Ni_pz_nc/postqeout2', v_bare, header)
 
 
 # plot_num = 11
-v_bare = compute_v_bare(ecutrho, alat, a[0], a[1], a[2], nr, atomic_positions,
-                        atomic_species, pseudodir)
-v_h = compute_v_h(charge, ecutrho, alat, b)
+v_bare = compute_v_bare(data.ecutrho, data.alat, data.a[0], data.a[1], data.a[2], data.nr,
+                        data.atomic_positions, data.atomic_species, pseudodir)
+v_h = compute_v_h(charge, data.ecutrho, data.alat, data.b)
 v_tot = v_bare + v_h
-write_charge('../tests/Ni_pz_nc/postqeout11', v_tot, header)
+write_charge('./Ni_pz_nc/postqeout11', v_tot, header)
 
 # plot_num = 1
 v_bare = compute_v_bare(
-    ecutrho, alat, a[0], a[1], a[2], nr, atomic_positions, atomic_species, pseudodir
+    data.ecutrho, data.alat, data.a[0], data.a[1], data.a[2], data.nr,
+    data.atomic_positions, data.atomic_species, pseudodir
 )
-v_h = compute_v_h(charge, ecutrho, alat, b)
+v_h = compute_v_h(charge, data.ecutrho, data.alat, data.b)
 charge_core = np.zeros(charge.shape)
-v_xc = compute_v_xc(charge, charge_core, str(functional))
+v_xc = compute_v_xc(charge, charge_core, str(data.functional))
 v_tot = v_bare + v_h + v_xc
-write_charge('../tests/Ni_pz_nc/postqeout1', v_tot, header)
+write_charge('./Ni_pz_nc/postqeout1', v_tot, header)
 
 
 
 #  Plotting tests, second step pp.x
+from postqe.plot import plotcharge1D, plotcharge2D
+from postqe.readutils import read_postqe_output_file
+from postqe.compute_vs import compute_G
 
-from plot import plotcharge1D, plotcharge2D
-from readutils import read_postqe_output_file, write_charge, create_header
-from compute_vs import compute_G
-
-charge = read_postqe_output_file('../tests/Ni_pz_nc/postqeout0')
+charge = read_postqe_output_file('./Ni_pz_nc/postqeout0')
 
 # Plot a 1D section
 x0 = [0., 0., 0.]
 e1 = [1., 0., 0.]
 nx = 50
-G = compute_G(b, charge.shape)
-fig = plotcharge1D(charge, G, a, x0, e1, nx)
+G = compute_G(data.b, charge.shape)
+fig = plotcharge1D(charge, G, data.a, x0, e1, nx)
 fig.show()
 
 # Plot a 2D section
@@ -80,8 +79,8 @@ e1 = [1., 0., 0.]
 nx = 50
 e2 = [0., 1., 0.]
 ny = 50
-G = compute_G(b, charge.shape)
-fig = plotcharge2D(charge, G, a, x0, e1, e2, nx, ny)
+G = compute_G(data.b, charge.shape)
+fig = plotcharge2D(charge, G, data.a, x0, e1, e2, nx, ny)
 fig.show()
 
 import time
