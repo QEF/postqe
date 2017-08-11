8867ef513162564244917557070aa295b8959df2
Mauro Palumbo <mauropalumbo75@gmail.com>
2017-08-11 11:50:14 +0200

    small changes in asecalc. Started implemented more methods to extract other properties

--- postqe/asecalc.py
+++ postqe/asecalc.py
@@ -43,6 +43,23 @@ class Espresso(Calculator):
         Calculator.__init__(self, restart, ignore_bad_restart_file, label,
                             atoms, **kwargs)
 
+        # Read here the xml file and store a dictionary for the output part (dout)
+        import xmlschema
+        filename = self.label + '.xml'
+
+        ##########################################################
+        # TODO for whatever reason this is not working now
+        # schemaLoc = xmlschema.fetch_schema(filename)
+        # xs = xmlschema.XMLSchema(schemaLoc)
+        #
+        # temporary local solution
+        xs = xmlschema.XMLSchema('schemas/qes.xsd')
+        ##########################################################
+
+        print("Reading xml file: ", filename)
+        d = xs.to_dict(filename)
+        self.dout = d["output"]
+
 
     def calculate(self, atoms=None, properties=['energy'], system_changes=[]):
         """
@@ -62,24 +79,75 @@ class Espresso(Calculator):
         if changed_parameters:
             self.reset()
 
+    def get_number_of_bands(self):
+        """Return the number of bands."""
+        return int(self.dout["band_structure"]["nbnd"])
+
+    def get_xc_functional(self):
+        """Return the XC-functional identifier.
+
+        'LDA', 'PBE', ..."""
+        return self.dout["dft"]["functional"]
+
+    def get_bz_k_points(self):
+        """Return all the k-points in the 1. Brillouin zone.
+
+        The coordinates are relative to reciprocal latice vectors."""
+        return np.zeros((1, 3))
+
+    def get_number_of_spins(self):
+        """Return the number of spins in the calculation.
+
+        Spin-paired calculations: 1, spin-polarized calculation: 2."""
+        return 1
+
+    def get_spin_polarized(self):
+        """Is it a spin-polarized calculation?"""
+        return False
+
+    def get_ibz_k_points(self):
+        """Return k-points in the irreducible part of the Brillouin zone.
+
+        The coordinates are relative to reciprocal latice vectors."""
+        return np.zeros((1, 3))
+
+    def get_k_point_weights(self):
+        """Weights of the k-points.
+
+        The sum of all weights is one."""
+        return np.ones(1)
+
+    def get_pseudo_density(self, spin=None, pad=True):
+        """Return pseudo-density array.
+
+        If *spin* is not given, then the total density is returned.
+        Otherwise, the spin up or down density is returned (spin=0 or
+        1)."""
+        return np.zeros((40, 40, 40))
+
+    def get_effective_potential(self, spin=0, pad=True):
+        """Return pseudo-effective-potential array."""
+        return np.zeros((40, 40, 40))
+
+    def get_pseudo_wave_function(self, band=0, kpt=0, spin=0, broadcast=True,
+                                 pad=True):
+        """Return pseudo-wave-function array."""
+        return np.zeros((40, 40, 40))
+
+    def get_eigenvalues(self, kpt=0, spin=0):
+        """Return eigenvalue array."""
+        return np.arange(42, float)
+
+    def get_occupation_numbers(self, kpt=0, spin=0):
+        """Return occupation number array."""
+        return np.ones(42)
+
+
+    def get_fermi_level(self):
+        """Return the Fermi level."""
+        return float(self.dout["band_structure"]["fermi_energy"]) * units.Ry
 
     def read_results(self):
-        import xmlschema
-
-        filename = self.label + '.xml'
-
-        ##########################################################
-        # TODO for whatever reason this is not working now
-        # schemaLoc = xmlschema.fetch_schema(filename)
-        # xs = xmlschema.XMLSchema(schemaLoc)
-        #
-        # temporary local solution
-        xs = xmlschema.XMLSchema('schemas/qes.xsd')
-        ##########################################################
-
-        print("Reading xml file: ", filename)
-        d = xs.to_dict(filename)
-        dout = d["output"]
-        self.results['energy'] = float(dout["total_energy"]["etot"]) * units.Ry
+        self.results['energy'] = float(self.dout["total_energy"]["etot"]) * units.Ry
 
 
--- postqe/test_ase.py
+++ postqe/test_ase.py
@@ -11,15 +11,18 @@ if __name__ == "__main__":
     from ase.visualize import view
 
     Ni2 = read_espresso_xml('Ni.xml')
+    #Ni2 = Atoms()
+    Ni2.set_calculator(Espresso(label = 'Ni'))
     print (Ni2.get_atomic_numbers())
     print (Ni2.get_cell(True))
     print (Ni2.get_positions())
     print (Ni2.get_volume())
     view(Ni2)
 
-    Ni2.set_calculator(Espresso(label = 'Ni'))
+
     #Ni2.calc.read_results()
     print(Ni2.get_potential_energy())
-    print(Ni2.get_potential_energy())
+    print(Ni2.calc.get_xc_functional())
+    print(Ni2.calc.get_fermi_level())
 
 
