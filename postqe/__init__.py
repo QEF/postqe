30829001748a9223d3843f0a8d80719585e32c71
Mauro Palumbo <mauropalumbo75@gmail.com>
2017-08-11 12:10:23 +0200

    func.mod added (necessary for compiling the fortran modules at least for now)

--- postqe/__init__.py
+++ postqe/__init__.py
@@ -1,9 +1,9 @@
 
 from .eos_postqe import fitEtotV
-#from .dos_postqe import compute_dos
-#from .bands import compute_bands
-#from .compute_vs import compute_v_bare, compute_v_h, compute_v_xc
-#from .api import get_charge, get_potential, compute_G
-#from .xmlfile import get_cell_data, get_calculation_data, get_band_strucure_data
-#from .plot import plot1D_FFTinterp, plot2D_FFTinterp, simple_plot_xy, multiple_plot_xy, plot_EV, plot_bands
-#from .pyqe import pyqe_getcelldms
+from .dos_postqe import compute_dos
+from .bands import compute_bands
+from .compute_vs import compute_v_bare, compute_v_h, compute_v_xc
+from .api import get_charge, get_potential, compute_G
+from .xmlfile import get_cell_data, get_calculation_data, get_band_strucure_data
+from .plot import plot1D_FFTinterp, plot2D_FFTinterp, simple_plot_xy, multiple_plot_xy, plot_EV, plot_bands
+from .pyqe import pyqe_getcelldms
