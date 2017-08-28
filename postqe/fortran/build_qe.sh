4a12740c6b7b0cbc642fb83b4d8e6c68b5cc8fbc
Davide Brunato <brunato@sissa.it>
2017-08-28 14:33:32 +0200

    Updated QE build script and Makefile and updated the PW xsd schema.
        modified:   examples/example2/Si.xml
        modified:   examples/example3/Si.xml
        modified:   postqe/fortran/Makefile
        modified:   postqe/fortran/build_qe.sh
        modified:   schemas/qes.xsd

--- postqe/fortran/build_qe.sh
+++ postqe/fortran/build_qe.sh
@@ -1,6 +1,16 @@
 #!/bin/bash
+#
+# Script for download and build a local copy Quantum ESPRESSO suite, with
+# Position Independent Code shared relocatable libraries.
+#
+# Copyright (c), 2016-2017, Quantum Espresso Foundation and SISSA (Scuola
+# Internazionale Superiore di Studi Avanzati). All rights reserved.
+# This file is distributed under the terms of the LGPL-2.1 license. See the
+# file 'LICENSE' in the root directory of the present distribution, or
+# https://opensource.org/licenses/LGPL-2.1
+#
 
-QE_SOURCE_URL="https://github.com/QEF/q-e/archive/585716d6435014dfc493abcd08294cd9f02eee30.zip"
+QE_SOURCE_COMMIT="585716d6435014dfc493abcd08294cd9f02eee30"
 QE_SOURCE_MD5SUM="05dede6dd93e4d69109062aa429161c9"
 
 # Move into build/ directory
@@ -21,7 +31,7 @@ then
    echo "QE source code already downloaded ..."
 else
    echo "Download QE source code ..."
-   curl -SL $QE_SOURCE_URL -o q-e.zip
+   curl -SL https://github.com/QEF/q-e/archive/$QE_SOURCE_COMMIT.zip -o q-e.zip
 fi
 
 echo -n "Checks MD5SUM of source archive ... "
@@ -36,31 +46,30 @@ fi
 
 if [ -e q-e ] && [ -d q-e ]
 then
-    read -p "Remove existing QE source files? " -n 1 -r
-    echo    # (optional) move to a new line
-    if [[ $REPLY =~ ^[Yy]$ ]]
+    # Checks the optional argument
+    if [[ $1 =~ ^[YyNn]$ ]]
+    then
+        REPLACE=$1
+    else
+        # No or wrong argument: arks for a choice
+        read -p "Replace QE source files and rebuild? [Y/N] " -n 1 -r
+        echo
+        REPLACE=$REPLY
+    fi
+
+    if [[ $REPLACE =~ ^[Yy]$ ]]
     then
         echo "Remove existing source files ..."
         rm -Rf q-e
     else
-        exit
+        exit 0
     fi
 fi
 
 echo "Extract source files from archive ..."
-unzip q-e.zip
-mv q-e-585716d6435014dfc493abcd08294cd9f02eee30/ q-e/
+unzip q-e.zip && mv q-e-$QE_SOURCE_COMMIT/ q-e/
 
 echo "Build Quantum Espresso modules ..."
 cd q-e
-TOPDIR=`pwd`
-
-#./configure DFLAGS="-D__FFTW -D__MPI -D__HDF5" CFLAGS=-fPIC FFLAGS="-g -fPIC" \
-#    IFLAGS="-I\$(TOPDIR)/include -I\$(TOPDIR)/FoX/finclude -I../include/ -I/usr/lib64/gfortran/modules/openmpi/"
-
-# Manually modify HDF5_LIB parameter into generated make.inc
-#HDF5_LIB="-L/usr/lib64/openmpi/lib/ -lhdf5_fortran -lhdf5hl_fortran"
-#sed -i "s,HDF5_LIB =,HDF5_LIB = $HDF5_LIB," make.inc
-
 ./configure DFLAGS="-D__FFTW" MPIF90=gfortran CFLAGS=-fPIC FFLAGS="-g -fPIC"
 make pw
