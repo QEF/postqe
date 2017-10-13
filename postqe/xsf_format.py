d2e03cb36facdb934e3d8d07601639ed325264d4
Mauro Palumbo <mauropalumbo75@gmail.com>
2017-10-13 11:46:04 +0200

    small fixes in xsf format

--- postqe/xsf_format.py
+++ postqe/xsf_format.py
@@ -21,20 +21,21 @@ def xsf_struct(struct_info):
 
     #TODO: check a vectors. It seems right here but it is different from pp.x (why?)
     xsf =  ' CRYSTAL\n PRIMVEC\n'
-    a = struct_info['a']*struct_info['alat']*BOHR_RADIUS_SI*1e9 # convert in Ang
+    fact = BOHR_RADIUS_SI*1e10
+    a = struct_info['a']*fact # convert in Ang
     for at in a:
         xsf += '{:15.9f} {:15.9f} {:15.9f}\n'.format(float(at[0]),float(at[1]),float(at[2]))
     xsf += ' PRIMCOORD\n'
     xsf += ' {:4d} 1\n'.format(int(struct_info['nat']))
     for at in struct_info['atomic_positions']:
-        xsf += ' {:3s}   {:15.9f} {:15.9f} {:15.9f}\n'.format(at['@name'], at['$'][0], at['$'][1], at['$'][2])
+        xsf += ' {:3s}   {:15.9f} {:15.9f} {:15.9f}\n'.format(at['@name'], at['$'][0]*fact, at['$'][1]*fact, at['$'][2]*fact)
     return xsf
 
 def xsf_datagrid_2d(Z, nx, ny, m1, m2, x0, e1, e2, struct_info):
 
     xsf = 'BEGIN_BLOCK_DATAGRID_2D\n2D_PWSCF\nDATAGRID_2D_UNKNOWN\n'
     xsf += '{:4d}  {:4d}\n'.format(nx, ny)
-    fact = struct_info['alat']*BOHR_RADIUS_SI*1e9
+    fact = struct_info['alat']*BOHR_RADIUS_SI*1e10
     for x in x0:
         xsf += '{:15.9f} '.format(x*fact)
     xsf += '\n'
@@ -44,10 +45,10 @@ def xsf_datagrid_2d(Z, nx, ny, m1, m2, x0, e1, e2, struct_info):
     for x in e2:
         xsf += '{:15.9f} '.format(x*m2*fact)
     xsf += '\n'
-    for j in range(0, ny-1):
-        for i in range(0, nx-1):
+    for j in range(0, ny):
+        for i in range(0, nx):
             xsf += '{:12.4E}'.format(Z[i, j].real)
-            if ((j * (nx-1) + i + 1) % 6) == 0:
+            if ((j * nx + i + 1) % 6) == 0:
                 xsf += '\n'
     xsf += '\n'
     xsf += 'END_DATAGRID_2D\nEND_BLOCK_DATAGRID_2D'
@@ -57,7 +58,7 @@ def xsf_datagrid_3d(W, nx, ny, nz, m1, m2, m3, x0, e1, e2, e3, struct_info):
 
     xsf = 'BEGIN_BLOCK_DATAGRID_3D\n3D_PWSCF\nDATAGRID_3D_UNKNOWN\n'
     xsf += '{:4d}  {:4d}  {:4d}\n'.format(nx, ny, nz)
-    fact = struct_info['alat']*BOHR_RADIUS_SI*1e9
+    fact = struct_info['alat']*BOHR_RADIUS_SI*1e10
     for x in x0:
         xsf += '{:15.9f} '.format(x*fact)
     xsf += '\n'
@@ -68,13 +69,13 @@ def xsf_datagrid_3d(W, nx, ny, nz, m1, m2, m3, x0, e1, e2, e3, struct_info):
         xsf += '{:15.9f} '.format(x*m2*fact)
     xsf += '\n'
     for x in e3:
-        xsf += '{:15.9f} '.format(x * m3 * fact)
+        xsf += '{:15.9f} '.format(x*m3*fact)
     xsf += '\n'
-    for k in range(0, nz-1):
-        for j in range(0, ny-1):
-            for i in range(0, nx-1):
+    for k in range(0, nz):
+        for j in range(0, ny):
+            for i in range(0, nx):
                 xsf += '{:12.4E}'.format(W[i, j, k].real)
-                if ((k * ((ny-1)*(nx-1)) + j * (nx-1) + i + 1) % 6) == 0:
+                if ((k * (ny*nx) + j * nx + i + 1) % 6) == 0:
                     xsf += '\n'
     xsf += '\n'
     xsf += 'END_DATAGRID_3D\nEND_BLOCK_DATAGRID_3D'
